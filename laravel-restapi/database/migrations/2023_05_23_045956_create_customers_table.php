c0d2aa2f25b3152441b70a09cfb1cedc8f878e06
Sesdika <sesdika.sansani@gmail.com>
2023-05-24 00:11:42 +0700

    [laravel-restapi] restapi code to create, retrieve, update, and delete user as customer

---
+++ laravel-restapi/database/migrations/2023_05_23_045956_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->date('birth_date');
+            $table->string('location');
+            $table->timestamp('last_sending')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
