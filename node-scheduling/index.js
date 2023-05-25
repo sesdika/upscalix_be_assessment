78b6e082287e536675f2fbfb4bd6c3e6bc565d94
Sesdika <sesdika.sansani@gmail.com>
2023-05-25 10:03:10 +0700

    add variable to adjust birthday-message-app path

--- node-scheduling/index.js
+++ node-scheduling/index.js
@@ -1,10 +1,12 @@
 const { exec } = require('child_process');
 const cron = require('node-cron');
 
+var simple_birthday_app_path = 'C:\\xampp\\htdocs\\simple-birthday-app\\index.php';
+
 // Schedule the execution of PHP script at a specific time
 cron.schedule('0 */1 * * *', () => {
   // Execute PHP script as a child process
-  exec('php C:\\xampp\\htdocs\\simple-birthday-app\\index.php', (error, stdout, stderr) => {
+  exec('php '+simple_birthday_app_path, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing PHP script: ${error.message}`);
       return;
